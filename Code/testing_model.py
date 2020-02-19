3f02470be9cb1a3570bae4aed882ab441f9f22af
Ashish Goswami <33857516+alphacoder01@users.noreply.github.com>
2020-02-19 18:54:53 +0530

    added some gesture functionality

--- Code/testing_model.py
+++ Code/testing_model.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.models import load_model
+import webbrowser
 import numpy as np
 import cv2
 import tensorflow as tf
@@ -20,6 +21,7 @@ def prepare_frame(img):
     # img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     return img.reshape(-1, IMG_SIZE,IMG_SIZE,1)
 
+C = 0
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 vid = cv2.VideoWriter('output.avi',fourcc,6,(640,480))
@@ -43,6 +45,25 @@ while True:
     prediction = model.predict([prepare_frame(final)])
     text = str((CATEGORIES[int(np.argmax(prediction[0]))]))
     print(text)
+    
+    if text == 'Youtube':
+        C +=1
+        if C >=20:
+            webbrowser.open("https://www.youtube.com/")
+            C = 0
+    if text =='Browser':
+        C +=1
+        if C >=20:
+            webbrowser.open('https:/google.com/')
+            time.sleep(5)
+            C = 0
+    if text == 'Close':
+        C +=1
+        if C >=50:
+            browserExe = "chrome.exe"
+            os.system("taskkill /f /im "+browserExe)
+            C =0
+
     cv2.putText(frame,text,(200,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA)
     cv2.imshow('frame',frame)
     
